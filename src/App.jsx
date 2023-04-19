233e2fe363f44bf839560e0da179c24a5464a070
“lukenilsson” <luke.nilsson@gmail.com>
2023-04-19 16:05:19 -0400

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Signup } from "./Signup";
+import { Login } from "./Login";
 
 function App() {
   return (
     <div>
       <Header />
       <Signup />
+      <Login />
       <Content />
       <Footer />
     </div>
